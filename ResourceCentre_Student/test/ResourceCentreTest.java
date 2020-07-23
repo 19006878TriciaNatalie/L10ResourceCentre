1dfdfc8dfca0383f07c9a92d0d97337acabfdc2e
19036764 <19036764@Baymax-V2.lan>
2020-07-23 18:32:01 +0800

    Test to see if the program work or not Jia Wei

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -52,6 +52,19 @@ public class ResourceCentreTest {
 	public void addChromebookTest() {
 		//fail("Not yet implemented");
 		// write your code here
+		//Adding a blank object into the list (Error)
+		cb2 = new Chromebook("", "", "");
+		ResourceCentre.addChromebook(chromebookList, cb2);
+		assertSame("Test to see if the program add the blank object",cb2,chromebookList.get(0));
+		
+		//Add 2 item into null list so list become 2 item inside (Normal)
+		cb2 = new Chromebook("CB0012", "SAMSUNG Chromebook 4+", "Win 10");
+		chromebookList.clear();
+		ResourceCentre.addChromebook(chromebookList, cb1);
+		ResourceCentre.addChromebook(chromebookList, cb2);
+		assertEquals("Test to see if the total number of item in the list becomes 2 ",2,chromebookList.size());
+
+		
 	}
 	
 	@Test
